7a9aadbe33552ed548edc2b711a4fb59f1155737
ИСТОРИН ВЛАД <xloksik3@gmail.com>
2020-10-31 14:01:22 +0200

    Добавьте файлы проекта.

---
+++ Sun/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sun
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(r1.Text);
+                double P = 1;
+                for (int k = 1; k <= n; k++)
+                {
+                    if (k != 7 && k != n)
+                    {
+                        P *= (Math.Pow(-1, k) * (k - 7) / (2 * (n - k)));
+                    }
+                }
+                r2.Text = (P).ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
